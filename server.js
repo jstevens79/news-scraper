39dd5f4041d3b560184ced6bab5e535590d68000
jstevens79 <jstevens79@users.noreply.github.com>
2019-05-26 10:20:16 -0500

    getting the server and handlebars setup

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+const routes = require('./controllers/index');
+app.use(routes);
+
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}.`))
